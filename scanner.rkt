9c3cde77aa6885723560c6326b9bbc9976838c93
zimhoff <90417867+zimhoff@users.noreply.github.com>
2022-10-09 22:33:21 -0500

    Done

--- scanner.rkt
+++ scanner.rkt
@@ -53,157 +53,117 @@
    [whitespace (make_token input-port)]
    ))
 
-(define tokens(make_token(open-input-file "input01.txt"))) ;testing
-tokens  ;testing
-
-;(define input-port (open-input-file fileName))
+;(define tokens(make_token(open-input-file "input02.txt"))) ;testing
+;tokens  ;testing
   
 (define (RPAR tokens)
   [cond
-    [(equal? (first tokens) ")" (rest tokens))]])
+    [(equal? (first tokens) `RPAR) (rest tokens)]
+  [else (print "2")]])
 
 (define (ass_op tokens)
   [cond
-    [(equal? (first tokens) ":=" (rest tokens))]])
+    [(equal? (first tokens) `:=) (rest tokens)]])
 
 (define (ID tokens)
   [cond
-    [(equal? (first tokens) "ID" (rest tokens))]])
+    [(equal? (first tokens) `ID) (rest tokens)]])
 
-(define (error)
-  (printf "error"))
-
-;(define ($$ tokens)
-  ;[cond
-    ;[(equal? (first tokens) "$$" (rest tokens))]])
+(define (done tokens)
+  [cond
+    [(equal? (first tokens) `$$) (printf "accept \n")]])
 
 (define (match expected tokens)
   (cond
     [(equal? (first tokens) expected)
       (rest tokens)]
-     [else print"error"]))
-    ;[(equal? (first tokens) tokens (rest tokens))]
+    [else (error"Syntax Error")]))
 
 (define (program tokens)
     (cond
-    ;(case (first(first tokens))
-      ;[(ID read write $$)(stmt_list (rest tokens))]
-      [(equal? (first tokens)`ID) (stmt_list tokens)]
-      [(equal? (first tokens)`read) (stmt_list tokens)]
-      [(equal? (first tokens)`write) (stmt_list tokens)]
-      [(equal? (first tokens)`$$) (stmt_list tokens)]
-      [(equal? (first tokens)`$$) (match `$$ tokens)]
-      [else (print "1")]))
+      [(equal? (first tokens)`ID) (done(stmt_list tokens))]
+      [(equal? (first tokens)`read) (done(stmt_list tokens))]
+      [(equal? (first tokens)`write) (done(stmt_list tokens))]
+      [(equal? (first tokens)`$$) (print"accept")]
+      [else (error"Syntax Error")]))
 
 (define (stmt_list tokens)
     (cond
-    ;(case (first (first tokens))
-      ;[(ID read write :=) (stmt_list(stmt (rest tokens)))]
-      ;[($$) tokens]
       [(equal? (first tokens)`ID) (stmt_list(stmt tokens))]
       [(equal? (first tokens)`read) (stmt_list(stmt tokens))]
       [(equal? (first tokens)`write) (stmt_list(stmt tokens))]
-      [(equal? (first tokens)`$$) (stmt_list(stmt tokens))]
-      [(equal? `$$ (first tokens)) tokens]
-      [else ( print"1")]))
+      [(equal? (first tokens)`$$) tokens]
+      [else (error "Syntax Error")]))
 
 (define (stmt tokens)
     (cond
-    ;(case (first (first tokens))
-      ;[(ID)(expr(ass_op(rest tokens)))]
-      ;[(read)(ID(rest tokens))]
-      ;[(write)(expr(rest tokens))]
       [(equal? (first tokens)'ID) (expr (match `:= (match `ID tokens)))]
       [(equal? (first tokens)`read) (match `ID (match `read tokens))]
       [(equal? (first tokens)`write) (expr (match `write tokens))]
-      [else (print "5")]))
+      [else (error"Syntax Error")]))
 
 (define (expr tokens)
     (cond
-    ;(case (first(first tokens))
-      ;[(ID INT RPAR)(term_tail (term(rest tokens)))]
       [(equal? (first tokens)`ID)(term_tail (term tokens))]
       [(equal? (first tokens)`INT)(term_tail (term tokens))]
       [(equal? (first tokens)`LPAR)(term_tail (term tokens))]
-      [else (error)]))
+      [else (error"Syntax Error")]))
 
 (define (term_tail tokens)
     (cond
-    ;(case (first(first tokens))
-      ;[(+ -)(term_tail (term(rest tokens)))]
-      ;[(RPAR ID read write $$) tokens]
-      [(equal? (first tokens)`+ )(term_tail (term tokens))]
-      [(equal? (first tokens)`- )(term_tail (term tokens))]
+      [(equal? (first tokens)`+ )(term_tail (term (add_op tokens)))]
+      [(equal? (first tokens)`- )(term_tail (term (add_op tokens)))]
       [(equal? (first tokens)`RPAR) tokens]
       [(equal? (first tokens)`ID) tokens]
       [(equal? (first tokens)`read) tokens]
       [(equal? (first tokens)`write) tokens]
       [(equal? (first tokens)`$$) tokens]
-      [else (error)]))
+      [else (error"Syntax Error")]))
 
 (define (term tokens)
     (cond
-    ;(case (first(first tokens))
-     ; [(ID INT LPAR)(factor_tail (factor(rest tokens)))]
       [(equal? (first tokens)`ID)(factor_tail (factor tokens))]
       [(equal? (first tokens)`INT)(factor_tail(factor tokens))]
       [(equal? (first tokens)`LPAR) (factor_tail(factor tokens))]
-      [else (error)]))
+      [else (error"Syntax Error")]))
 
 (define (factor_tail tokens)
     (cond
-    ;(case (first (first tokens))
-      ;[(* /)(factor_tail (factor(rest tokens)))]
-      ;[(+ - RPAR ID read write $$)tokens]
-      [(equal? (first tokens)`*) (factor_tail (factor(rest tokens)))]
-      [(equal? (first tokens)`/) (factor_tail (factor(rest tokens)))]
-      [(equal? (first tokens) `+) (rest tokens)]
-      [(equal? (first tokens) `-) (rest tokens)]
+      [(equal? (first tokens)`*) (factor_tail (factor(mult_op tokens)))]
+      [(equal? (first tokens)`/) (factor_tail (factor(mult_op tokens)))]
+      [(equal? (first tokens) `+) tokens]
+      [(equal? (first tokens) `-) tokens]
       [(equal? (first tokens)`RPAR) tokens]
       [(equal? (first tokens)`ID) tokens]
       [(equal? (first tokens)`read) tokens]
       [(equal? (first tokens)`write) tokens]
       [(equal? (first tokens)`$$) tokens]
-      [else (error)]))
+      [else (error"Syntax Error")]))
 
 (define (factor tokens)
    (cond
-   ;(case (first(first tokens))
-     ;[(LPAR) (RPAR(expr (rest tokens)))]
-     ;[(ID INT)(rest tokens)]
-    [(equal? (first tokens) `LPAR) (RPAR(expr (rest tokens)))]
-    [(equal? (first tokens) `ID) (rest tokens)]
-    [(equal? (first tokens) `INT) (rest tokens)]
-    [else(error)]))
+    [(equal? (first tokens) `ID) (match `ID tokens)]
+    [(equal? (first tokens) `INT) (match `INT tokens)]
+    [(equal? (first tokens) `LPAR) (match `RPAR (expr (rest tokens)))]
+    [else (error"Syntax Error")]))
 
 (define (add_op tokens)
     (cond
-    ;(case (first(first tokens))
-     ;[(+ -) (rest tokens)]
-     [(equal? (first tokens) `+) tokens]
-     [(equal? (first tokens) `-) tokens]
-     [else (error)]))
+     [(equal? (first tokens) `+) (rest tokens)]
+     [(equal? (first tokens) `-) (rest tokens)]
+     [else (error"Syntax Error")]))
 
 (define (mult_op tokens)
      (cond
-     ;(case first tokens)
-    ;[(* /) (rest tokens)]
-    [(equal? (first tokens) `*) tokens]
-    [(equal? (first tokens) `/) tokens]
-    [else (error)]))
+    [(equal? (first tokens) `*) (rest tokens)]
+    [(equal? (first tokens) `/) (rest tokens)]
+    [else (error"Syntax Error")]))
 
 (define (parse input-file)
  (program (make_token (open-input-file input-file)))) 
 
-;(define (parse fileName)
-  ;(make_token (open-input-file fileName))))
-;(parse "input01.txt")
-
-  ;(define (parse tokens)
-   ;(program tokens)
-    ;(define tokens (tokenizer(open-input-file "input01.txt"))))
-
-
-;procedure match(expected)
-;if input token = expected then consume input token()
-;else parse error
+(parse "input01.txt")
+(parse "input02.txt")
+(parse "input03.txt")
+(parse "input04.txt")
+(parse "input05.txt")
